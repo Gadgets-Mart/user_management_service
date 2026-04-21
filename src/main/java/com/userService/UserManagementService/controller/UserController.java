4bc1040775f0cc800fb6d0b1c5ae2a08ad6b822a
Bhatte <2487391@cognizant.com>
2026-04-21 18:23:59 +0530

    user entity created

--- src/main/java/com/userService/UserManagementService/controller/UserController.java
+++ src/main/java/com/userService/UserManagementService/controller/UserController.java
@@ -1,4 +1,6 @@
 package com.userService.UserManagementService.controller;
 
 public class UserController {
+
 }
+
--- src/main/java/com/userService/UserManagementService/model/UserModel.java
+++ src/main/java/com/userService/UserManagementService/model/UserModel.java
@@ -1,15 +1,53 @@
 package com.userService.UserManagementService.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="users")
+@EntityListeners(AuditingEntityListener.class)
 public class UserModel {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable=true,length =20)
     private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private  String role;
+    @Column(nullable = false,length = 10)
+    private String phoneNo;
+    private  String address;
+    private String city;
+    private  String state;
+    private  int pincode;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Instant createdAt;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+
+
+    //getter setter
+
+
 }
