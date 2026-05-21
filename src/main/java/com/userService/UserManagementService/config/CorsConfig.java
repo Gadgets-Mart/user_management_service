b28beb688f8b6d0a4c3010534c8d83d8622b947c
2487390 <PrajwalR.shettar@cognizant.com>
2026-05-21 18:24:50 +0530

    Added proper domain for cros

--- src/main/java/com/userService/UserManagementService/config/CorsConfig.java
+++ src/main/java/com/userService/UserManagementService/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**") //to allow all the backend api
-                        .allowedOrigins("http://localhost:4200")  //to allow frontend
+                        .allowedOrigins("http://localhost:4200","http://localhost:4201") //to allow other domain and avoid cros origin
                         .allowedMethods("GET","PUT","POST","DELETE","PATCH") //to allow the request methods
                         .allowedHeaders("*")//to allow all headers Authorazation,Client_name,type ect
                          .allowCredentials(true);  //to allow JWt /Auth headers
